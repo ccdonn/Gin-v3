d000dc4d9b3640ff8f8f10cd0e4dce846c5b854f
donn.lin <donn.lin@DonnLinde-MacBook-Pro.local>
2019-03-29 12:59:00 +0800

    remove unwanted code

--- src/main/router.go
+++ src/main/router.go
@@ -1,19 +1,13 @@
 package main
 
 import (
-	"container/list"
-	"database/sql"
 	"fmt"
-	"log"
-	"time"
 
 	// . "../config"
 	middleware "../middleware"
-	. "../response"
 	service "../service"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 func routers() {
@@ -54,126 +48,6 @@ func routers() {
 		// tutorial.POST("/", service.CreateTutorial)
 		// tutorial.PUT("/:ID", service.UpdateTutorial)
 		// tutorial.DELETE("/:ID", service.DeleteTutorial)
-	}
-
-	me := router.Group("/me")
-	{
-		me.GET("/", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"name": "name",
-			})
-		})
-
-		me.GET("/dbdirect", func(c *gin.Context) {
-			db, err := sql.Open("mysql", "root:root@/promotion")
-			log.Println(db)
-			rows, err := db.Query("select agent_id, nickname, username from account where agent_id < ?", 1000)
-			if err != nil {
-				log.Fatal("sql error")
-			}
-			fmt.Println(rows)
-			var (
-				agentID  int32
-				nickname string
-				username string
-			)
-
-			res := list.New()
-			for rows.Next() {
-				err := rows.Scan(&agentID, &nickname, &username)
-				if err != nil {
-					log.Fatal("row scan error")
-					c.JSON(500, "Error")
-					return
-				}
-
-				res.PushBack(MeResponse{
-					AgentID:  agentID,
-					Username: username,
-					Nickname: nickname,
-				})
-
-			}
-
-			slice := make([]MeResponse, res.Len())
-
-			cnt := 0
-			for iter := res.Front(); iter != nil; iter = iter.Next() {
-				slice[cnt] = iter.Value.(MeResponse)
-				cnt++
-			}
-
-			c.JSON(200, gin.H{
-				"status":    "success",
-				"Timestamp": time.Now(),
-				// "result": MeResponse{
-				// 	AgentID:  agentID,
-				// 	Username: username,
-				// 	Nickname: nickname,
-				// },
-				"size":   cnt,
-				"result": slice,
-			})
-
-		})
-
-		me.GET("/db", func(c *gin.Context) {
-			// db := GetConn()
-			db, err := sql.Open("mysql", "root:root@/promotion")
-			if err != nil {
-				c.JSON(500, gin.H{
-					"error": "db connection error",
-				})
-				return
-			}
-
-			rows, err := db.Query("select agent_id, nickname, username from account where agent_id = ?", 19)
-			if err != nil {
-				// log.Panic(err)
-				c.JSON(500, gin.H{
-					"error": "sql error",
-				})
-				return
-			}
-
-			var (
-				agentID  int32
-				nickname string
-				username string
-			)
-
-			log.Println(rows)
-
-			if rows.Next() {
-				err = rows.Scan(&agentID, &nickname, &username)
-				if err != nil {
-					// log.Panic("scan error")
-					c.JSON(500, gin.H{
-						"error": "scan error",
-					})
-					return
-				}
-			} else {
-				c.JSON(200, gin.H{
-					"status":    "success",
-					"Timestamp": time.Now(),
-					"size":      0,
-					"result":    nil,
-				})
-				return
-			}
-
-			c.JSON(200, gin.H{
-				"status":    "success",
-				"Timestamp": time.Now(),
-				"size":      1,
-				"result": MeResponse{
-					AgentID:  agentID,
-					Username: username,
-					Nickname: nickname,
-				},
-			})
-		})
 	}
 
 }
